0a6b198846eede1af80a65fd972076be72be12f9
songohg <songogh94@gmail.com>
2022-10-05 19:45:43 +0900

    기본적인 게시물 작성,리스트 구현

--- src/com/KoreaIT/example/JAM/Article.java
+++ src/com/KoreaIT/example/JAM/Article.java
@@ -1,5 +1,18 @@
 package com.KoreaIT.example.JAM;
 
-public class Article {
+public class Article extends Object {
 
+	int id;
+	String title;
+	String body;
+	public Article(int id, String title, String body) {
+		 this.id = id;
+		 this.title = title;
+		 this.body = body;
+	}
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+	}
+	
 }
--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,14 +1,52 @@
 package com.KoreaIT.example.JAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 public static void main(String[] args) { 
 		 Scanner sc = new Scanner(System.in);
 		 
+		 List<Article> articles = new ArrayList<>();
+		 
+		 int lastArticleId = 0;
+		 
 		 while(true) {
 			 System.out.printf("명령어) ");
 			 String cmd = sc.nextLine().trim();
+			 
+			 if(cmd.equals("article wrtie")) {
+				 System.out.println("==게시물 작성 ==");
+				 
+				 int id = lastArticleId +1;				 
+				 System.out.printf("제목 : ");
+				 String title = sc.nextLine();
+				 System.out.printf("내용 : ");
+				 String body = sc.nextLine();
+				 
+				 Article article = new Article(id, title, body);
+				 articles.add(article);
+				 lastArticleId++;
+				 
+//				System.out.println(article);
+				 
+				 System.out.printf("%d번 글이 생성 되었습니다 \n", id);			 
+			 }else if(cmd.equals("article list")) {
+				 System.out.println("== 게시물 리스트 ==");
+				 
+				 if(articles.size() == 0) {
+					 System.out.println("게시물이 없습니다");
+					 continue;
+				 }
+				 
+				 System.out.println("번호   |   제목");
+				 
+				 for(Article article : articles) {
+					 System.out.printf("%d  |  %s\n", article.id, article.title);
+					 
+				 }
+			 }
 			 
 			 if(cmd.equals("exit")) {
 				 System.out.println("프로그램을 종료합니다");
@@ -18,4 +56,5 @@ public static void main(String[] args) {
 		 }
 		 sc.close();
 	}
+
 }
